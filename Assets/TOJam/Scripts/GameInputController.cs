5db88b5735f8f018e762d3b528a4b71590c811f3
Marc <marccrobbins@gmail.com>
2020-05-08 20:40:48 -0400

    Set up input registration

---
+++ Assets/TOJam/Scripts/GameInputController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Rewired;
+using UnityEngine;
+
+public class GameInputController : MonoBehaviour
+{
+    private const InputActionEventType BUTTON_TYPE = InputActionEventType.ButtonJustReleased;
+    private const InputActionEventType AXIS_TYPE = InputActionEventType.AxisActive;
+    
+    private Player _player;
+
+    private void Start()
+    {
+        _player = ReInput.players.GetPlayer(0);
+
+        if (_player == null) return;
+     
+    }
+
+    private void OnLeftJoystickHorizontal(InputActionEventData data)
+    {
+        Debug.Log("Left arm move horizontal");
+    }
+    
+    private void OnLeftJoystickVertical(InputActionEventData data)
+    {
+        Debug.Log("Left arm move vertical");
+    }
+    
+    private void OnRightJoystickHorizontal(InputActionEventData data)
+    {
+        Debug.Log("Right arm move horizontal");
+    }
+    
+    private void OnRightJoystickVertical(InputActionEventData data)
+    {
+        Debug.Log("Rgiht arm move vertical");
+    }
+    
+    private void OnLeftTrigger(InputActionEventData data)
+    {
+        Debug.Log("Left trigger");
+    }
+    
+    private void OnRightTrigger(InputActionEventData data)
+    {
+        Debug.Log("Right trigger");
+    }
+    
+    private void OnMenu(InputActionEventData data)
+    {
+        Debug.Log("Open menu");
+        if (_player == null) return;
+
+        _player.controllers.maps.SetMapsEnabled(true, "Menu");
+        _player.controllers.maps.SetMapsEnabled(false, "InGame");
+    }
+}
---
+++ Assets/TOJam/Scripts/MenuInputController.cs
@@ -0,0 +1,52 @@
+﻿using Rewired;
+using UnityEngine;
+
+public class MenuInputController : MonoBehaviour
+{
+    private const InputActionEventType BUTTON_TYPE = InputActionEventType.ButtonJustReleased;
+    private const InputActionEventType AXIS_TYPE = InputActionEventType.AxisActive;
+    
+    private Player _player;
+
+    private void Start()
+    {
+        _player = ReInput.players.GetPlayer(0);
+
+        if (_player == null) return;
+        
+        _player.AddInputEventDelegate(OnMenuMoveHorizontal, UpdateLoopType.Update, AXIS_TYPE, "MoveHorizontal");
+        _player.AddInputEventDelegate(OnMenuMoveVertical, UpdateLoopType.Update, AXIS_TYPE, "MoveVertical");
+        _player.AddInputEventDelegate(OnExit, UpdateLoopType.Update, BUTTON_TYPE, "Exit");
+        _player.AddInputEventDelegate(OnSelect, UpdateLoopType.Update, BUTTON_TYPE, "Select");
+        _player.AddInputEventDelegate(OnBack, UpdateLoopType.Update, BUTTON_TYPE, "Back");
+    }
+    
+    private void OnMenuMoveHorizontal(InputActionEventData data)
+    {
+        Debug.Log("move horizontal");
+    }
+    
+    private void OnMenuMoveVertical(InputActionEventData data)
+    {
+        Debug.Log("move vertical");
+    }
+
+    private void OnExit(InputActionEventData data)
+    {
+        Debug.Log("exit menu");
+        if (_player == null) return;
+
+        _player.controllers.maps.SetMapsEnabled(false, "Menu");
+        _player.controllers.maps.SetMapsEnabled(true, "InGame");
+    }
+    
+    private void OnSelect(InputActionEventData data)
+    {
+        Debug.Log("select");
+    }
+    
+    private void OnBack(InputActionEventData data)
+    {
+        Debug.Log("back");
+    }
+}
