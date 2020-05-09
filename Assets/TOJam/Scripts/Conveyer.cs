0a7e7d9ab8e31bf59079c80cecb1d5b678fc2127
Dylan <dwyllace@gmail.com>
2020-05-08 22:49:28 -0400

    Added conveyer tag for conveyerables & full test scene for puzzle

--- Assets/TOJam/Scripts/Conveyer.cs
+++ Assets/TOJam/Scripts/Conveyer.cs
@@ -12,13 +12,9 @@ public class Conveyer : MonoBehaviour
 
 	private void OnTriggerStay(Collider other)
 	{
-		other.transform.position = Vector3.MoveTowards(other.transform.position, transform.position + (transform.forward * 1000), Velocity * Time.deltaTime);
-		/*
-		Debug.Log("Triggerstay! "+other.gameObject.name);
-		Rigidbody body = other.attachedRigidbody;
-		Vector3 forcePoint = other.ClosestPoint(CollisionRef.position);
-		Vector3 velocityAdd = this.transform.forward * Velocity * Time.deltaTime;
-		body.AddForceAtPosition(velocityAdd, forcePoint, ForceMode.VelocityChange);
-		*/
+		if (other.gameObject.CompareTag("Conveyer"))
+		{
+			other.transform.position = Vector3.MoveTowards(other.transform.position, transform.position + (transform.forward * 1000), Velocity * Time.deltaTime);
+		}
 	}
 }
