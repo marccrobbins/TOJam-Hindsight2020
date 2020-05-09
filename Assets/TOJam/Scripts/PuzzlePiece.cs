bbc52e44eba288dd660e3e05bc3f2abb67486904
Dylan <dwyllace@gmail.com>
2020-05-08 22:12:05 -0400

    Added basic piece mechanic

---
+++ Assets/TOJam/Scripts/PuzzlePiece.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour
+{
+	public string pieceName;
+	[Range(1,100)] public float pieceCollisionSize;
+}
---
+++ Assets/TOJam/Scripts/PuzzleSlot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleSlot : MonoBehaviour
+{
+	private PuzzlePiece matchingPiece;
+	[SerializeField] private SphereCollider sphere;
+
+	public void StartTrackingPiece(PuzzlePiece pieceToEnable)
+	{
+		matchingPiece = pieceToEnable;
+		sphere.radius = matchingPiece.pieceCollisionSize / 200f;
+		matchingPiece.gameObject.SetActive(false);
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		PuzzlePiece piece = other.GetComponent<PuzzlePiece>();
+		if (piece == null) return;
+
+		if(matchingPiece.pieceName == piece.pieceName)
+		{
+			piece.gameObject.SetActive(false);
+			matchingPiece.gameObject.SetActive(true);
+			gameObject.SetActive(false);
+		}
+	}
+}
